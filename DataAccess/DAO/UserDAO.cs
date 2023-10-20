03a4f0be556bf6a01ddecf23e91b1453a3003468
HaiLeVN <lehai112003@gmail.com>
2023-10-20 22:35:58 +0700

    added user login function, get user role function for win app

---
+++ DataAccess/DAO/UserDAO.cs
@@ -0,0 +1,57 @@
+﻿using BusinessObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DAO
+{
+    public class UserDAO
+    {
+        // Using Singleton pattern
+        private static UserDAO instance = null;
+        private static object instanceLook = new object();
+
+        public static UserDAO Instance
+        {
+            get
+            {
+                lock (instanceLook)
+                {
+                    if (instance == null)
+                    {
+                        instance = new UserDAO();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public IEnumerable<User> GetAllMember()
+        {
+            IEnumerable<User> users = null;
+
+            try
+            {
+                var context = new CageShopUniContext();
+                users = context.Users;
+                
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return users;
+        }
+
+        public User Login(string username, string password)
+        {
+            IEnumerable<User> userList = GetAllMember();
+            User user = userList.SingleOrDefault(us => us.UserName.Equals(username, StringComparison.Ordinal) && us.UserPassword.Equals(password, StringComparison.Ordinal));
+            return user;
+        }
+    }
+}
---
+++ DataAccess/Repository/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using BusinessObject.Models;
+using DataAccess.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        public IEnumerable<User> GetAllUser()
+        {
+            return UserDAO.Instance.GetAllMember();
+        }
+
+        public User Login(string username, string password)
+        {
+            return UserDAO.Instance.Login(username, password);
+        }
+    }
+}
