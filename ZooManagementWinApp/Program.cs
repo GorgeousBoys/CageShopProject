06e962ea8afd86f87de7123e3c40de2259d19d4c
HaiLeVN <lehai112003@gmail.com>
2023-11-06 22:35:48 +0700

    fixed reference

--- ZooManagementWinApp/Program.cs
+++ ZooManagementWinApp/Program.cs
@@ -1,4 +1,6 @@
-namespace ZooManagementWinApp
+using SalesWinApp;
+
+namespace CageManagementWinApp
 {
     internal static class Program
     {
@@ -11,7 +13,7 @@ namespace ZooManagementWinApp
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new frmLogin());
         }
     }
 }
