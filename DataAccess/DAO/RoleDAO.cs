453bf933bc96a6701da598b8f8172864314159da
NhanDz <lmnhn995@gmail.com>
2023-11-16 16:18:41 +0700

    create roleDao roleRepo

---
+++ DataAccess/DAO/RoleDAO.cs
@@ -0,0 +1,33 @@
+﻿using BusinessObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DAO
+{
+    public class RoleDAO
+    {
+        private static RoleDAO instance = null;
+        private static object instanceLook = new object();
+        public static RoleDAO Instance { 
+            get
+            {
+                lock (instanceLook)
+                {
+                    if (instance == null)
+                    {
+                        instance = new RoleDAO();
+                    }
+                    return instance;
+                }
+            }
+        }
+        
+        public List<Role> GetAll()
+        {
+            return new CageShopUni_Context().Roles.ToList();
+        }
+    }
+}
--- Repository/Repository/IUserRepository.cs
+++ Repository/Repository/IUserRepository.cs
@@ -10,13 +10,14 @@ namespace Repository.Repository
     public interface IUserRepository 
     {
         public IEnumerable<User> GetAllUser();
+        public List<User> GetUsers();
         public User Login(string username, string password);
         public string getUserRole(string username);
         public void registerShopKeeperAccount (User user);
         public User getUserByUserId(int id);
 
         void AddUser(User user, string selectedImagePath);
-        void UpdateUser(User user);
+        void UpdateUser(User user, string selectedImagePath);
         void DeleteUser(User user);
 
         User FindByID(int id);
